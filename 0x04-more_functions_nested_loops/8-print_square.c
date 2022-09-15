0900b367cf52f39d1693215260d530dcfee64411
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-16 02:45:16 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,15 +6,13 @@
 void print_square(int size)
 {
 	int l, w;
-
+	
 	if (size > 0)
 	{
 		for (l = 1; l <= size; l++)
 		{
 			for (w = 1; w <= size; w++)
-			{
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
 	}
