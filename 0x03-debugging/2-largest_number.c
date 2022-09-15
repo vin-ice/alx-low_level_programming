79a8317612fb55e98855b10638393baae9ff0e1f
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-15 11:18:58 +0300

    Update 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,21 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if ((a > b && a > c) || (a == b && a > c) || (a == c && a >b))
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if ((b > a && b > c) || (b == c && b > a))
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
-	{
-		largest = c;
-	}
 	else
 	{
-		largest = a;
+		largest = c;
 	}
 	return (largest);
 }
