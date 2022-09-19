e5c6cd70fcda5296eca409e82a3832372b89422f
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-20 00:58:14 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,15 +5,24 @@
  */
 void rev_string(char *s)
 {
-	char collect[1000];
-	int count = 0;
+	int a, len;
 
-	while (*(s + count) != '\0')
+	char *begin, *end = s;
+
+	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
 	{
-		collect[count] = *(s + count);
-		count++;
+		end++;
 	}
-	
-	while (--count >= 0)
-		_putchar(collect[count]);
+	len = a + 1;
+	begin = s;
+	for (a = 0; a < len / 2; a++)
+	{
+		char x;
+		x = *end;
+		*end = *begin;
+		*begin = x;
+		begin++;
+		end--;
+	}
+	end[len + 1] = '\0';
 }
