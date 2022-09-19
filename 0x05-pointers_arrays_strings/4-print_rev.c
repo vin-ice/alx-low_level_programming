e86d2a2a7276e49bb2a049611a311e7c25c01945
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-19 20:48:44 +0300

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@ void print_rev(char *s)
        char collect[100];
 	
 	while (count != '\0')
-		collect[count++] = *(s + counter); 
+		collect[count++] = *(s + count); 
 	while (count >= 0)
 		_putchar(collect[count--]);
 	_putchar('\n');
