94ebb84a48f631882b91bacf498b299510ee9be2
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-19 21:21:50 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,12 @@ void puts2(char *str)
 {
 	int counter = 0;
 
-	while (*(str++) != '\0')
+	while (*(str) != '\0')
 	{
 		if (counter % 2 == 0)
 			_putchar(*str);
 		counter++;
+		str++;
 	} ;
 	_putchar('\n');
 }
