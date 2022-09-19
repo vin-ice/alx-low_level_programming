9d2ba501f7e974ed45cb64e7d0f8624d6c7306b9
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-20 01:25:47 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,13 +6,25 @@
  */
 int _atoi(char *s)
 {
-      if (extract_num(*s))
-      {
-      
-      } 
-	return (0);
-}
-int extract_num(*s)
-{
-	while ()
+	int sign;
+	unsigned int num;
+	char *temp;
+
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
 }
