ec4d3d30b2024ae50093aee721e3c4e9cf4ae4c8
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-20 01:20:56 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 /**
  * print_array - prints n elements of an array
  * @a: array to be evaluated
- * n: number o characters to print out
+ * @n: number of characters to print out
  */
 void print_array(int *a, int n)
 {
