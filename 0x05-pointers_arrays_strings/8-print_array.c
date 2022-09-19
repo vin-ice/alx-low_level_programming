d1329f8163a1739457130a1e1a66415af6be74fa
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-19 22:02:31 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,10 +10,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= n; i++)
 	{
-		printf("%d", a[i]);
-		if (i < n)
+		if (a[i])
 		{
-			printf(", ");
+			printf("%d", a[i]);
+			if (i < n)
+			{
+				printf(", ");
+			}
 		}
 	}
 	printf("\n");
