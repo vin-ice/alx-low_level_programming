83a8e73ce8956c4e8b688fa736568b5bb4a701d5
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-22 17:49:37 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,11 +5,17 @@
  */
 void print_number(int n)
 {
-	int m = n, multi;
-
-	while (n)
-	{
-		if ()
-	}
+	unsigned int num;
+	num = n;
 	
+	if (n < 0)
+	{
+		_putchar(45);
+		num = -n;
+	}
+	if (num / 10)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
 }
