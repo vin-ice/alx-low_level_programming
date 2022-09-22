7d3cf29ea01fac984286e0b7edf47f47bd2c2da9
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-22 14:41:20 +0300

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,13 +7,10 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	while (n > 0)
-	{
-		if (*src == '\0')
-			break;
-		*dest++ = *src++;
-		n--;
-	}
-
-	return dest;
+	int byteCount;
+	for (byteCount = 0; byteCount < n && src[byteCount] != '\0'; byteCount++)
+		dest[byteCount] = src[byteCount];
+	for (; byteCount < n; byteCount++)
+		dest[byteCount] = '\0';
+	return (dest);
 }
