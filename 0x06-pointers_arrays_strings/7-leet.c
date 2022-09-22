921afbf3fb7c5d772c74fbeb560e347be9e7d87b
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-22 17:23:33 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,7 +4,23 @@
  * Return: formatted string
  */
 char *leet(char *n)
-{
-	
-	return n;
+{int stringCount, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
+
+	stringCount = 0;
+	while (s[stringCount] != '\0')
+	{
+		leetCount = 0;
+		while (leetCount < 10)
+		{
+			if (leetLetters[leetCount] == s[stringCount])
+			{
+				s[stringCount] = leetNums[leetCount];
+			}
+			leetCount++;
+		}
+		stringCount++;
+	}
+	return (s);
 }
