484eb77f2cf29bdc15809ab4f5c824c6aaf6c02b
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-22 20:13:31 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,24 @@
-/**
- * cap_string - capitalizes all words of a string
- * @n: String
- * Return: returns string array
- */
-char *cap_string(char *n)
-{
-	int  h_sep = 0;
-	int i = 0;
-	int n_val;
+#include <stdio.h>
 
-	while (n[i])
+/**
+ * *cap_string - this is awesome
+ * @s: pointer to char params
+ *
+ * Return: *s
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char delimeters[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[0] >= 97 && s[0] <= 122)
-		{
 			s[0] = s[0] - 32;
-		}
-		if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-		    || s[count] == ',' || s[count] == ';' || s[count] == '.'
-		    || s[count] == '.' || s[count] == '!' || s[count] == '?'
-		    || s[count] == '"' || s[count] == '(' || s[count] == ')'
-		    || s[count] == '{' || s[count] == '}')
-		{
-			if (s[count + 1] >= 97 && s[count + 1] <= 122)
-			{
-				s[count + 1] = s[count + 1] - 32;
-			}
-		}
-		count++
-		i++;
+			for (j = 0; delimeters[j] != '\0'; j++)
+				if (s[i] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+					s[i + 1] = s[i + 1] - 32;
 	}
-	return n;
+	return (s);
 }
