ca3718cc0686b0dde58a42afee76ee498ff9e07d
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-22 17:31:55 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,18 +5,18 @@
  */
 char *rot13(char *n)
 {
-	int n_val;
+	int n_val, i = 0;
 
-	while (*n)
+	while (n[i] != '\0')
 	{
-		n_val = (int) *n;
+		n_val = (int) n[i];
 
 		if (n_val > 64 && n_val < 91)
-			*n = (char) 65 + ((n_val - 65 + 13) % 26);
+			n[i] = (char) (65 + (n_val - 65 + 13) % 26);
 		else if (n_val > 96 && n_val < 123)
-			*n = (char) 97 + ((n_val - 97 + 13) % 26);
-		n++;
+			n[i] = (char) (97 + (n_val - 97 + 13) % 26);
+		i++;
 	}
 
-	return n;
+	return (n);
 }
