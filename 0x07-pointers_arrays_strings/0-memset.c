ace601662f5297a15bcbae7b04e670219c27ba12
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-27 16:55:46 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,14 @@
 /**
- * _memset - fills memory with a constant byte
- * @s: address of memory area
- * @b: constant to copy
- * @n: size of memory area to occupy
- *
- * Return: returns pointer to memory area 
- */
-char *_memset(char *s, char b, unsigned int n)
-{	
-	while (n--)
+* _memset - fills memory with a constant byte
+* @s: address of memory area
+* @b: constant to copy
+* @n: size of memory area to occupy
+*
+* Return: returns pointer to memory area
+*/
+ char *_memset(char *s, char b, unsigned int n)
+ {
+ 	while (n--)
 		*(s + n) = b;
 	return (s);
 }
