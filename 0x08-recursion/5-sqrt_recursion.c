4713066d733009e6a96d4141db31684879f21c83
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-28 20:32:52 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,28 @@
 /**
- * _sqrt_recursion - returns natural square root of a number
- * @n: number
- * Return: returns -1 if n does not have natural square root
+ * sqrt_check - checks for the square root of c
+ * @g:guess at sqrt
+ * @c: number to find sqrt of
+ *
+ * Return: -1 or sqrt of c
+ */
+int sqrt_check(int g, int c)
+{
+	if (g * g == c)
+		return (g);
+	if (g * g > c)
+		return (-1);
+	return (sqrt_check(g + 1, c));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer to find sqrt of
+ *
+ * Return: natural square root or -1
  */
 int _sqrt_recursion(int n)
 {
-	if ()
+	if (n == 0)
+		return (0);
+	return (sqrt_check(1, n));
 }
