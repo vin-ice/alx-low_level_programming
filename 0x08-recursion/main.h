468931c467bb45adb96e465fa4fd365779d8aa72
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-28 19:45:57 +0300

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,7 +3,7 @@
 
 void _putchar(char);
 void _puts_recursion(char *s);
-void _print_rev_recursion(char *s)
+void _print_rev_recursion(char *s);
 int _strlen_recursion(char);
 int factorial(int);
 int _pow_recursion(int, int);
