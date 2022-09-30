56d547383e5167fc9282c1f5e697791d3b01164f
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-30 23:21:59 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,44 +1,21 @@
-#include "main.h"
-#include <unistd.h>
+
+
+#include <stdio.h>
+
 /**
- * main - driver function
- * @argc: count of arguments
- * @argv: pointer to arguments
- * Return: returns 0
- * if error returns 1 
+ * main - prints all arguments it recieves
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
-	args(argc, argv);
-	return (0);
-}
-/**
- * args - prints all arguments
- * @s: size/count
- * @p:pointer to arguments list
- */
-void args(int s, char *p[])
-{
-	int i = 0, j = 0;
+	int i;
 
-	while (i < s)
+	for (i = 0; i < argc; i++)
 	{
-		while (p[i][j])
-		{
-			_putchar(p[i][j]);
-			j++;
-		}
-		_putchar('\n');
-		j = 0;
-		i++;
+		printf("%s\n", argv[i]);
 	}
-}
-/**
- * _putchar - prints character to terminal
- * @c: character
- * Return: returns 1 on success
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+	return (0);
 }
