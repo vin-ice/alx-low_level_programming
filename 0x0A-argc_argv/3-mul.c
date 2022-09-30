a1a10dfc779db53b5e109a04613a58d8a1b85ba4
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-30 23:23:04 +0300

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,71 +1,25 @@
-#include "main.h"
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int _atoi(char *s);
 /**
- * main - prints product of two arguments
- * @argc: argument count
- * @argv: pointer to arguments list
- * Return: returns 1 if arguments are missing
+ * main - multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	char *err = "Error\0";
+	int a, b, c;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
-		while (*err)
-		{
-			_putchar(*err);
-			err++;
-		}
-		_putchar('\n');
+		puts("Error");
 		return (1);
 	}
-	mul(argv);
-	_putchar('\n');
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+	printf("%d\n", c);
 	return (0);
-}
-/**
- * mul - multiplies arguments
- * @p: pointer to arguments
- */
-void mul(char *p[])
-{
-	int prod = (_atoi(p[1]) * _atoi(p[2]));
-	
-	printf("%d", prod);
-}
-/**
- * _putchar - prints a character
- * @c:character
- * Return: returns 1 if successful
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
- * _atoi - converts string to integer
- * @s: string
- * Return: returns integer
- */
-int _atoi(char *s)
-{
-	int i = 0, neg = 1;
-
-	while (*s)
-	{
-		if ('-')
-		{
-			neg *= -1;
-			continue;
-		}
-		i = i * 10 + *s - '0';
-		s++;
-	}
-	if (neg)
-		i = i * -1;
-	return (i);
 }
