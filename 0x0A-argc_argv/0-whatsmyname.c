33dceb46a4720cd4a622b8c9e519c10806013b13
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-30 23:17:37 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -18,14 +18,3 @@ int main(int __attribute__ ((unused)) argc, char *argv[])
 	_putchar('\n');
 	return (0);
 }
-
-/**                                                                                                   
- *  *_putchar - writes the character c to stdout
- *  @c: The character to print
- *  Return: success 1.
- *  On error, -1 is returned, and errno is set appropriately.
- *  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-} 
