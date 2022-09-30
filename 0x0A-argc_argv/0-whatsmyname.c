61bc62eb1b73290b95d7ec78a35ec0c35b6f20c1
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-09-30 23:16:53 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,24 +10,15 @@
  */
 int main(int __attribute__ ((unused)) argc, char *argv[])
 {
-	whats_my_name(argv[0]);	
-	return (0);
-}
-/**
- * whats_my_name - prints name of program
- * @str: first parameter
- */ 
-void whats_my_name(char *str)
-{
-	int i = 0;
-
 	while (str[i])
 	{
 		_putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');
+	return (0);
 }
+
 /**                                                                                                   
  *  *_putchar - writes the character c to stdout
  *  @c: The character to print
