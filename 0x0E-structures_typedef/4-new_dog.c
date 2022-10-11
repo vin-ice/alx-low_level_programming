e36495a8e2e2d8bf28c842b85a1302ab2ddf7867
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-10-11 15:02:41 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,11 +9,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog = malloc(sizeof *dog);
+	unsigned int nl, ol, i;
+	dog_t *dog;
+	
+	if (*name == NULL || *owner == NULL)
+		return (NULL);
+	dog = malloc(sizeof *dog);
 	if (dog == NULL)
 		return (NULL);
-	dog->name = name;
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
 	dog->age = age;
-	dog->owner = owner;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
 	return (dog);
 }
