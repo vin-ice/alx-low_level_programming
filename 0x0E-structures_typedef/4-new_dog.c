0f93894d6ffd3e49cd6660087234db8ef7d86f8f
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-10-11 15:07:31 +0300

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,20 +1,22 @@
-#include "dog.h"
 #include <stdlib.h>
+#include "dog.h"
+
 /**
- * new_dog - creates new dog
- * @name: name
- * @age: age
- * @owner: owner
- * Return: returns new dog
+ * new_dog - creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: pointer to new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int nl, ol, i;
 	dog_t *dog;
-	
-	if (*name == NULL || *owner == NULL)
+
+	if (name == NULL || owner == NULL)
 		return (NULL);
-	dog = malloc(sizeof *dog);
+	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
 	for (nl = 0; name[nl]; nl++)
