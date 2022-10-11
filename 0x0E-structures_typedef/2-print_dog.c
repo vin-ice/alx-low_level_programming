7f438a0da7befd1541f719f2794a88811506d40a
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-10-11 14:58:13 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,16 +1,15 @@
 #include "dog.h"
 #include <stdio.h>
-#include <stdlib.h>
 /**
- * print_dog - prints a structure dog
- * @d: structure
- */
+* print_dog - prints a structure dog
+* @d: structure
+*/
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
-		printf("Age: %s\n", (!d->age) ? "(nil)" : ftoa(d->age));
+		printf("Age: %.6f\n", d->age);
 		printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
 	}
 	
