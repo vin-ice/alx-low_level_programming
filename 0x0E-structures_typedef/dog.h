878400419d0c6340e3b247fec910720f63f1b7d9
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-10-11 14:53:07 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
+/**
+*struct dog - dog object
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*/
 struct dog{
 	char *name;
 	float age;
 	char *owner;
 };
+/**
+* dog_t - typedef for struct dog
+*/
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
