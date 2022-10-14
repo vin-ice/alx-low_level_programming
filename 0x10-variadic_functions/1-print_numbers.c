e77fcab9660a0d8b8d1c144bc8ba0b4d6dd32887
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-10-14 16:28:24 +0300

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdarg.h>
+#include "variadic_functions.h"
 /**
 *print_numbers - prints numbers, followed by a new line
 *@separator: seperates numbers
