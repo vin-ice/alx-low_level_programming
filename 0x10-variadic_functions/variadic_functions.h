406b05b4992e99157c075e8841afdda488f90dff
Vincent Wanjohi <vwan045@gmail.com>
2022-10-17 14:49:04 +0300

    Variadic correction

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,21 @@
 #ifndef _VARIADIC_H_
 #define _VARIADIC_H_
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int, ...);
+void print_numbers(const char *, const unsigned int, ...);
+void print_strings(const char *, const unsigned int, ...);
 /**
  * struct print - print type with corresponding print function
  * @t: print type
  * @f: print function
  */
-typedef struct print
+struct print
 {
 	char *t;
-	void (*f)(va_list);
-} print_t;
-int sum_them_all(const unsigned int, ...);
-void print_numbers(const char *, const unsigned int, ...);
-void print_strings(const char *, const unsigned int, ...);
+	void (*f)(va_list p);
+};
+typedef struct print print_t;
 void print_all(const char * const format, ...);
+
 #endif
