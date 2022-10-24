9501fafd4a24f0fd4da6d17912cd96ad2317fd69
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-10-24 16:47:36 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,9 +12,7 @@ size_t print_list(const list_t *h)
     for(; h; len++)
     {
         if (h->str == NULL)
-        {
             printf("[0] (nil)\n");
-        }
         else
             printf("[%d] %s\n", h->len, h->str);
         h = h->next;
