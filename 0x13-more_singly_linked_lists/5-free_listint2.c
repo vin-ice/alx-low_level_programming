3b16ffaff3f237b02d4b578e0d4d70ac5e7505b5
vin-ice <57555841+vin-ice@users.noreply.github.com>
2022-10-27 11:45:37 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
     
     if (head == NULL)
            return;
-    while (head == NULL)
+    while (*head != NULL)
     { 
         next = (*head)->next;
         free (*head);
