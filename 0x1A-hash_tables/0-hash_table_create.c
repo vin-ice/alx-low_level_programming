18339cfeaf4542ea95fb23b352138076d83bf3fc
vin-ice <57555841+vin-ice@users.noreply.github.com>
2023-01-08 18:18:44 +0300

    0-corredction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,10 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
     hash_table->size = size;
     hash_table->array = malloc(sizeof(hash_node_t) * hash_table->size);
     if (hash_table->array == NULL)
-    {
-        free(hash_table);
         return (NULL);
-    }
     
     for (i = 0; i < hash_table->size; i++)
         hash_table->array[i] = NULL;
