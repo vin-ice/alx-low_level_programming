4ca1e39cbdcbf5adcd3f836ddff26d6ff1b2df15
vin-ice <57555841+vin-ice@users.noreply.github.com>
2023-01-08 18:33:08 +0300

    4-co

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
     unsigned long int index = 0;
     hash_node_t *node;
 
+    if (ht == NULL || key == NULL || *key == '\0')
+        return (NULL);
     index = key_index((const unsigned char *)key, ht->size);
     node = ht->array[index];
     while (node)
